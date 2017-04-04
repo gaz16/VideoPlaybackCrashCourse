45767d4193f66ee26d242546712a31ad91df6002
Brian Pollack <brian@pollack.tech>
2017-04-04 19:04:16 -0400

    Fixed video lagging bug

--- Video Playback/Video Playback/ViewController.swift
+++ Video Playback/Video Playback/ViewController.swift
@@ -8,45 +8,26 @@
 
 import UIKit
 
-//Added
 import AVKit
 import AVFoundation
-//
 
 class ViewController: UIViewController {
 
-    //Added
     @IBAction func playVideo(_ sender: Any) {
-        guard let url = URL(string: "https://devimages.apple.com.edgekey.net/samplecode/avfoundationMedia/AVFoundationQueuePlayer_HLS2/master.m3u8") else {
-            return
-        }
-        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
-        let player = AVPlayer(url: url)
-        
-        //Create an AVPlayer, passing it local URL
-        let theURL = Bundle.main.url(forResource:"test", withExtension: "mov");
+        let theURL = Bundle.main.url(forResource:"stockvideo", withExtension: "mp4");
         let avPlayer = AVPlayer.init(url: theURL!)
         let avPlayerLayer = AVPlayerLayer(player: avPlayer)
         avPlayerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
         avPlayer.actionAtItemEnd = AVPlayerActionAtItemEnd.none
         
-        avPlayerLayer.frame = view.layer.bounds
-        view.backgroundColor = UIColor.clear
-        view.layer.insertSublayer(avPlayerLayer, at:0)
-        
-        
-        
-        
         // Create a new AVPlayerViewController and pass it a reference to the player.
         let controller = AVPlayerViewController()
         controller.player = avPlayer
         
-        // Modally present the player and call the player's play() method when complete.
         present(controller, animated: true) {
-            player.play()
+            avPlayer.play()
         }
     }
-    //
     
     override func viewDidLoad() {
         super.viewDidLoad()
